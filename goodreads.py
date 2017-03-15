745f499971ccace3afbb45e888a8d597ee168c62
dsb_mac <dsbmach@gmail.com>
2017-03-15 13:07:45 -0400

    refactor fetch_goodreads()

--- goodreads.py
+++ goodreads.py
@@ -17,6 +17,7 @@ from variety.plugins.IQuoteSource import IQuoteSource
 
 logger = logging.getLogger("variety")
 
+
 class GoodreadsSource(IQuoteSource):
     """
         Retrives quotes from goodreads.com. Reads the popular quotes.
@@ -24,10 +25,11 @@ class GoodreadsSource(IQuoteSource):
         Attributes:
             quotes(list): list containing the quotes
     """
+
     def __init__(self):
         super(IQuoteSource, self).__init__()
         self.quotes = []
-    
+
     @classmethod
     def get_info(cls):
         return {
@@ -42,7 +44,7 @@ class GoodreadsSource(IQuoteSource):
 
     def activate(self):
         if self.active:
-            return        
+            return
         self.active = True
 
         self.quotes = []
@@ -53,56 +55,59 @@ class GoodreadsSource(IQuoteSource):
         self.active = False
 
     def fetch_goodreads_quotes(self):
-        baseUrl = 'https://www.goodreads.com/quotes'
+        BASE_URL = 'https://www.goodreads.com/quotes'
 
-        self.quotes = [];
+        self.quotes = []
 
         # iterate through goodreads pagination
         for i in range(1, 20):
             query = '?page=' + str(i)
-            url = baseUrl + query
+            url = BASE_URL + query
             bs = Util.html_soup(url)
 
             # this is the element that contains the quote text
-            quoteElems = bs.find_all("div", { "class" : "quoteText" })
+            quoteElems = bs.find_all("div", {"class": "quoteText"})
 
-            #process the selected elems to get the quote text
-            for tag in quoteElems:                        
+            # process the selected elems to get the quote text
+            for tag in quoteElems:
                 # ignore the tag that contains the author info
                 quote_stripped_author = tag.contents[:-1]
                 quoteFragments = []
-                quoteText = ""
-
-                # the quote might be seperated by several <br> 
-                # have to traverse them to gather up the quote text pieces
-                for quote_pieces in quote_stripped_author:
-                    fragment = ''
-                    try:
-                        fragment = quote_pieces.text.strip()
-                    except:
-                        fragment = quote_pieces.strip()
-                    quoteFragments.append(fragment)                    
-
+                self._create_quote_fragments(quoteFragments, quote_stripped_author)
+                
                 # create a new entry
-                new_quote = self.assemble_quote(tag, quoteFragments)
+                new_quote = self.assemble_quote(tag, quoteFragments, url)
                 self.quotes.append(new_quote)
 
         if not self.quotes:
-            logger.warning("Could not find quotes for URL " + baseUrl)
+            logger.warning("Could not find quotes for URL " + BASE_URL)
 
-    def assemble_quote(self, tag, quoteFragments):
+    def _create_quote_fragments(self, quoteFragments, quote_stripped_author):
+        # the quote might be seperated by several <br>
+        # have to traverse them to gather up the quote text pieces
+        for quote_pieces in quote_stripped_author:
+            fragment = ''
+            try:
+                fragment = quote_pieces.text.strip()
+            except:
+                fragment = quote_pieces.strip()
+            quoteFragments.append(fragment)
+
+    def assemble_quote(self, tag, quoteFragments, url):
         # there is a unicode  hyphen that seperates the quote and author
         # we want to extract only the quote by splitting
         quoteText = " ".join(quoteFragments)
         splitQuote = quoteText.split(u"\u2015")
 
-        #this gets rid of the unicode quotations and stuff
-        #TODO need an algo to detect multiple quotes to ignore their removal
+        # this gets rid of the unicode quotations and stuff
+        # TODO need an algo to detect multiple quotes to ignore their removal
         quoteText = splitQuote[0].encode("ascii", "ignore")
-        quoteText = ' '.join(quoteText.split())                
-        author = tag.find("a", { "class" : "authorOrTitle"}).contents[0].strip().encode("ascii", "ignore")
-        newItem = {"quote": quoteText, "author": author, "sourceName": "goodreads", "link": url}
-       
+        quoteText = ' '.join(quoteText.split())
+        author = tag.find("a", {"class": "authorOrTitle"}).contents[
+            0].strip().encode("ascii", "ignore")
+        newItem = {"quote": quoteText, "author": author,
+                   "sourceName": "goodreads", "link": url}
+
         return newItem
 
     def get_for_author(self, author):
@@ -116,26 +121,3 @@ class GoodreadsSource(IQuoteSource):
             return [random.choice(self.quotes)]
         else:
             return self.quotes
-
-
-### BEGIN LICENSE
-# Copyright (c) 2017 Denis Mach
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
-### END LICENSE
